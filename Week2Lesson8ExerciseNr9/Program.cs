7ae130b07d8fd7a51cfd1e493711515a781c425f
konrad101296 <konradslimak@o2.pl>
2024-03-08 19:39:37 +0100

    Add project files.

---
+++ Week2Lesson8ExerciseNr9/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("enter a number");
+int number = int.Parse(Console.ReadLine());
+List<int> list = new List<int>();
+for (int i = 0; i < number; number /= 2)
+{
+
+    if (number % 2 == 0)
+    {
+        list.Add(0);
+    }
+    else if (number % 2 == 1)
+    {
+        list.Add(1);
+    }
+}
+list.Reverse();
+foreach (int i in list)
+{
+    Console.Write(i);
+}
